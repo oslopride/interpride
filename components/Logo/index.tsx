53687b8d1e22b1d550d477ccbc57180694fd7c41
Boye Borg <boye.borg@icloud.com>
2019-09-10 20:03:11 +0200

    Update logo (#1)
    * turn off props validation
    
    * add new logo
    
    * update logo component

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -1,25 +1,20 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import Link from "next/link";
 import styled from "styled-components";
 
-const LogoImage = styled.img`
-  margin-right: 10px;
-  height: 100px;
-`;
+type Props = {
+  classname?: string;
+};
 
-const LogoContainer = styled.a`
-  display: flex;
-  align-items: center;
-  font-size: 35px;
-`;
-
-const Logo = () => {
+const Logo: FunctionComponent<Props> = ({ classname }) => {
   return (
-    <Link href="/" passHref>
-      <LogoContainer aria-label="Go to the homepage">
-        <LogoImage src="/static/logo.svg" alt="Interpride AGM logo" />
-        INTERPRIDE
-      </LogoContainer>
+    <Link href="/">
+      <a className={classname} aria-label="Go to the homepage">
+        <img
+          src="/static/logo.svg"
+          alt="Interpride AGM logo. Squiggely rainbow over the text 'interpride'."
+        />
+      </a>
     </Link>
   );
 };
