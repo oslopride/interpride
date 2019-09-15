e04923c82ef9dff41de65f205bd6e962a0452341
Boye Borg <boye.borg@icloud.com>
2019-09-15 17:16:16 +0200

    add schedule page

--- pages/schedule.tsx
+++ pages/schedule.tsx
@@ -1,5 +1,29 @@
 import React from "react";
 
-const Schedule = () => <div>Schedule</div>;
+const Schedule = () => (
+  <div>
+    <h1>Schedule</h1>
+    <p>The complete schedule is not yet ready, and will be released later.</p>
+    <h2>World Pride 2021 kick-off</h2>
+    <p>
+      During the conference, we will host a special nightclub event on the
+      Saturday at Rockefeller Concert Hall, a four story building with a
+      capacity of 2000 people. This will be a closing Party and kickoff for
+      WorldPride 2021 in Copenhagen.
+    </p>
+    <p>
+      All conference attendees will have free entry to the event and receive
+      drink vouchers. The event is inspired by our closing night which we have
+      hosted on the parade day every year since 1982.
+    </p>
+    <p>
+      In addition to giving the conference attendees a good night out, the event
+      will also be a major source of income for Oslo Pride enabling us to
+      balance our budget. The club event has been sold out each year Oslo Pride
+      have hosted it and we are confident that it will be yet another success
+      during InterPride AGM.
+    </p>
+  </div>
+);
 
 export default Schedule;
