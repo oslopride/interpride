7efc6b4fb755fe4778865ac082c508cae919ed87
Boye Borg <boye.borg@icloud.com>
2019-09-15 13:53:13 +0200

    make font loading non-blocking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,19 @@
 import App from "next/app";
-import Head from "next/head";
 import React from "react";
 
 export default class NextApp extends App {
+  componentDidMount() {
+    import("webfontloader").then(WebFont => {
+      WebFont.load({
+        google: {
+          families: ["Montserrat:600,700", "Open+Sans:400,400i,700,700i"]
+        }
+      });
+    });
+  }
+
   render() {
     const { Component, pageProps } = this.props;
-    return (
-      <>
-        <Head>
-          <link
-            key="google-fonts"
-            href="https://fonts.googleapis.com/css?family=Montserrat:600,700|Open+Sans:400,400i,700,700i&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <Component {...pageProps} />
-      </>
-    );
+    return <Component {...pageProps} />;
   }
 }
