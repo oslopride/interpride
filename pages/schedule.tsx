6593f1cf859b7dfcc2e393fe36c96cb569060830
Boye Borg <boye.borg@icloud.com>
2019-09-15 18:51:53 +0200

    update schedule (#8)

--- pages/schedule.tsx
+++ pages/schedule.tsx
@@ -8,20 +8,9 @@ const Schedule = () => (
     <p>
       During the conference, we will host a special nightclub event on the
       Saturday at Rockefeller Concert Hall, a four story building with a
-      capacity of 2000 people. This will be a closing Party and kickoff for
-      WorldPride 2021 in Copenhagen.
-    </p>
-    <p>
-      All conference attendees will have free entry to the event and receive
-      drink vouchers. The event is inspired by our closing night which we have
-      hosted on the parade day every year since 1982.
-    </p>
-    <p>
-      In addition to giving the conference attendees a good night out, the event
-      will also be a major source of income for Oslo Pride enabling us to
-      balance our budget. The club event has been sold out each year Oslo Pride
-      have hosted it and we are confident that it will be yet another success
-      during InterPride AGM.
+      capacity of 2000 people. This will be the kickoff for WorldPride 2021. All
+      conference attendees will have free entry to the event and receive drink
+      vouchers.
     </p>
   </div>
 );
