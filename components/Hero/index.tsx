ff32983ee78bf55d832bae5b241a3239a5696d55
Boye Borg <boye.borg@icloud.com>
2019-09-17 20:10:04 +0200

    typo

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
         <S.ContentBox>
           <h2>Registration</h2>
           <p>
-            The super early bird tickets will be available on <b>October 17</b>.
+            The super early-bird tickets will be available on <b>October 17</b>.
             A complete price list are avalable on{" "}
             <Link href="/registration">
               <a aria-label="Go to the registration page">
