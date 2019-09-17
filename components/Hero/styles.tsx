48ddf6299795dec07b790f6d2a94304f65b0caef
Boye Borg <boye.borg@icloud.com>
2019-09-17 20:07:32 +0200

    improve hero styling

--- components/Hero/styles.tsx
+++ components/Hero/styles.tsx
@@ -3,73 +3,43 @@ import styled from "styled-components";
 export const Hero = styled.div`
   color: white;
   background-color: #05264c;
-  background-repeat: no-repeat;
-  background-size: 80vw;
-  background-position: right bottom -20vw;
-  font-family: Montserrat;
+  padding: 4rem 1rem 7rem 1rem;
 `;
 
-export const Date = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  min-height: 10vw;
-  padding: 3rem 0 30vw;
-  text-align: center;
-  text-transform: uppercase;
-
-  @media (min-width: 620px) {
-    padding: 3rem 0 15vw;
-  }
-
+export const Title = styled.div`
   h1 {
-    margin: 1rem;
+    font-family: Montserrat, sans-serif;
+    font-size: 2.5rem;
+    text-align: center;
 
-    @media (min-width: 620px) {
+    @media (min-width: 500px) {
       font-size: 3rem;
     }
   }
 `;
 
-export const Links = styled.div`
-  position: relative;
-  z-index: 1;
+export const Content = styled.div`
   display: flex;
   justify-content: center;
-  align-items: baseline;
   flex-flow: row wrap;
-  margin-top: -40vw;
-  padding: 4rem 1rem;
-  min-height: 10vw;
+  margin-top: 5rem;
   text-align: center;
 
-  @media (min-width: 620px) {
-    margin-top: -20vw;
-    padding-top: 10rem;
-    padding-bottom: 10rem;
+  @media (min-width: 900px) {
+    margin-top: 7rem;
+  }
+`;
+
+export const ContentBox = styled.div`
+  margin: 1rem;
+  max-width: 340px;
+  line-height: 1.5;
+
+  b {
+    color: #fec101;
   }
 
-  @media (min-width: 920px) {
-    padding-top: 10rem;
-  }
-
-  @media (min-width: 1120px) {
-    padding-top: 10rem;
-    padding-bottom: 10rem;
-  }
-
-  div {
-    margin: 1rem;
-    max-width: 340px;
-    cursor: pointer;
-
-    @media (min-width: 620px) {
-      margin-right: 2rem;
-      margin-left: 2rem;
-    }
-
-    p {
-      font-family: Open Sans;
-    }
+  a {
+    color: white;
   }
 `;
