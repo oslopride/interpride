7bfcc6940a10c482f3ea02a1a8fe57fa0d64a7e4
Siddise Hirpa <siddisehirpa@gmail.com>
2019-09-17 23:36:16 +0200

    Fix text spacing and h2 font in PageContent

--- components/PageContent/index.tsx
+++ components/PageContent/index.tsx
@@ -4,4 +4,12 @@ export const PageContent = styled.div`
   max-width: 1000px;
   margin: 0 auto;
   padding: 2rem;
+  line-height: 2rem;
+
+  h2 {
+    font-family: Montserrat, sans-serif;
+    font-weight: 600;
+    margin-top: 2rem;
+    margin-bottom: 1rem;
+  }
 `;
