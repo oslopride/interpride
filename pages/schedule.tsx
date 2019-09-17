253d1810f0e05861126caa0b108bacb163bb2c33
Siddise Hirpa <siddisehirpa@gmail.com>
2019-09-17 20:42:53 +0200

    Fix text styling on sub pages (#11)
    * Add new PageTitle component
    
    * Remove color prop to have same background color on all pages and add title to all pages
    
    * Add pagecontent component to center text on pages
    
    * Change text align and padding of title to match content

--- pages/schedule.tsx
+++ pages/schedule.tsx
@@ -1,18 +1,22 @@
 import React from "react";
+import PageTitle from "../components/PageTitle";
+import * as S from "../components/PageContent";
 
 const Schedule = () => (
-  <div>
-    <h1>Schedule</h1>
-    <p>The complete schedule is not yet ready, and will be released later.</p>
-    <h2>World Pride 2021 kick-off</h2>
-    <p>
-      During the conference, we will host a special nightclub event on the
-      Saturday at Rockefeller Concert Hall, a four story building with a
-      capacity of 2000 people. This will be the kickoff for WorldPride 2021. All
-      conference attendees will have free entry to the event and receive drink
-      vouchers.
-    </p>
-  </div>
+  <>
+    <PageTitle title="Schedule"></PageTitle>
+    <S.PageContent>
+      <p>The complete schedule is not yet ready, and will be released later.</p>
+      <h2>World Pride 2021 kick-off</h2>
+      <p>
+        During the conference, we will host a special nightclub event on the
+        Saturday at Rockefeller Concert Hall, a four story building with a
+        capacity of 2000 people. This will be the kickoff for WorldPride 2021.
+        All conference attendees will have free entry to the event and receive
+        drink vouchers.
+      </p>
+    </S.PageContent>
+  </>
 );
 
 export default Schedule;
--- pages/venue.tsx
+++ pages/venue.tsx
@@ -1,67 +1,71 @@
 import React from "react";
 import Link from "next/link";
+import PageTitle from "../components/PageTitle";
+import * as S from "../components/PageContent";
 
 const Venue = () => (
-  <div>
-    <img src="/static/clarion-hotel-oslo.jpg" alt="Hotel illustration" />
-    <div>
-      <h1>Clarion Hotel Oslo</h1>
-      <ul>
-        <li>
-          <a
-            href="https://www.google.com/maps/?q=59.9078,10.7564272"
-            aria-label="Google maps"
-          >
-            Dronning Eufemias Gate 15
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.nordicchoicehotels.com/hotels/norway/oslo/clarion-hotel-oslo/"
-            aria-label="The hotel website"
-          >
-            Hotel website
-          </a>
-        </li>
-      </ul>
-      <p>
-        The hotel is located in the city center just a few steps from the
-        Central Station, next to the Oslo Opera House and the Oslofjord, where
-        the delegates will arrive from the airport within 19 minutes on the
-        Airport Express Train (included in the conference registration fee). All
-        the LGBTQI establishments in Oslo are within walking distance of the
-        conference hotel. Choice Hotel Bjørvika is brand new, and opens in April
-        2019.
-      </p>
-      <h2>Food</h2>
-      <p>
-        Norwegian hotels are famous for their breakfasts. You may choose between
-        a variety of different options, from full English breakfast to a light
-        French breakfast or a traditional Norwegian breakfast. There is also a
-        variety of vegetarian and vegan options to choose from.
-      </p>
-      <p>Lunch and dinner are included in the registration fee.</p>
-      <h2>Conference Facilities</h2>
-      <p>
-        The conference venue includes a plenary room with a capacity of 380
-        people, 3 meetings rooms with a capacity of 60 people, 4 rooms with a
-        capacity of 40 people and 4 board rooms with a capacity of 20 people.
-      </p>
-      <p>
-        All conference venues are wheelchair friendly. Internet is free for all
-        conference attendees.
-      </p>
-      <h2>Pricing</h2>
-      <p>
-        1,590 NOK (USD 199) for a double room per night including breakfast
-        (taxes included). Head over to{" "}
-        <Link href="/registration">
-          <a>the registration page</a>
-        </Link>{" "}
-        to see what{"'"}s included in the registration fee.
-      </p>
-    </div>
-  </div>
+  <>
+    <PageTitle title="Clarion Hotel Oslo"></PageTitle>
+    <S.PageContent>
+      <img src="/static/clarion-hotel-oslo.jpg" alt="Hotel illustration" />
+      <div>
+        <ul>
+          <li>
+            <a
+              href="https://www.google.com/maps/?q=59.9078,10.7564272"
+              aria-label="Google maps"
+            >
+              Dronning Eufemias Gate 15
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.nordicchoicehotels.com/hotels/norway/oslo/clarion-hotel-oslo/"
+              aria-label="The hotel website"
+            >
+              Hotel website
+            </a>
+          </li>
+        </ul>
+        <p>
+          The hotel is located in the city center just a few steps from the
+          Central Station, next to the Oslo Opera House and the Oslofjord, where
+          the delegates will arrive from the airport within 19 minutes on the
+          Airport Express Train (included in the conference registration fee).
+          All the LGBTQI establishments in Oslo are within walking distance of
+          the conference hotel. Choice Hotel Bjørvika is brand new, and opens in
+          April 2019.
+        </p>
+        <h2>Food</h2>
+        <p>
+          Norwegian hotels are famous for their breakfasts. You may choose
+          between a variety of different options, from full English breakfast to
+          a light French breakfast or a traditional Norwegian breakfast. There
+          is also a variety of vegetarian and vegan options to choose from.
+        </p>
+        <p>Lunch and dinner are included in the registration fee.</p>
+        <h2>Conference Facilities</h2>
+        <p>
+          The conference venue includes a plenary room with a capacity of 380
+          people, 3 meetings rooms with a capacity of 60 people, 4 rooms with a
+          capacity of 40 people and 4 board rooms with a capacity of 20 people.
+        </p>
+        <p>
+          All conference venues are wheelchair friendly. Internet is free for
+          all conference attendees.
+        </p>
+        <h2>Pricing</h2>
+        <p>
+          1,590 NOK (USD 199) for a double room per night including breakfast
+          (taxes included). Head over to{" "}
+          <Link href="/registration">
+            <a>the registration page</a>
+          </Link>{" "}
+          to see what{"'"}s included in the registration fee.
+        </p>
+      </div>
+    </S.PageContent>
+  </>
 );
 
 export default Venue;
