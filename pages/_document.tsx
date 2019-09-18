f17ddc9f4c9ecbdfd515db44b082501a32a4940f
Boye Borg <boye.borg@icloud.com>
2019-09-18 08:02:57 +0200

    bugfix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,7 +32,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <title>Interpride AGM 2020<\title>
+          <title>Interpride AGM 2020</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
         <body>
