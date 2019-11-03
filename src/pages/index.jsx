7cd946ef01158ec7e74f6139e745f9bc8c591b1e
Boye Borg <boye.borg@icloud.com>
2019-11-03 15:48:46 +0100

    render front page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import { graphql } from "gatsby";
+import Page from "../templates/page";
 
-const Index = () => (
-	<>
-		<Header />
-		<p>Main page</p>
-		<Footer />
-	</>
-);
+export default function FrontPage({ data: { frontPage } }) {
+	return <Page data={{ page: frontPage }} />;
+}
 
-export default Index;
+export const query = graphql`
+	query SanityFrontPagesQuery {
+		frontPage: sanityFrontPage(_id: { eq: "global-frontPage" }) {
+			_rawLayout
+		}
+	}
+`;
