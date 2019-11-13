7f7b0dda3bbf6a103104cc478e209305c07d84b2
Christian Knappskog <christiankn@users.noreply.github.com>
2019-11-13 18:48:00 +0100

    Add privacy policy link to footer (#27)

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import * as S from "./styles";
+import { Link } from "gatsby";
 
 const Footer = () => (
 	<S.Footer>
 		<span>Created by Oslo Pride</span>
 		<span>
+			<Link to="/privacy-policy" aria-label="Privacy Policy">
+				Privacy Policy
+			</Link>
 			<a href="https://www.interpride.org/">InterPride</a>
 			<a href="http://epoa.eu/">EPOA</a>
 		</span>
