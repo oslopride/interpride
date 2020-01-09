b482707517bc2cc9c45b4d38b6123dd05a8971e4
Boye Borg <boye.borg@icloud.com>
2020-01-09 22:48:12 +0100

    don't introduce new bug

--- src/components/Logo/styles.jsx
+++ src/components/Logo/styles.jsx
@@ -2,9 +2,10 @@ import styled from "styled-components";
 
 export const LogoLink = styled.span`
 	cursor: pointer;
-	max-width: 400px;
 
 	img {
 		margin: 0;
+		width: 100%;
+		max-width: 400px;
 	}
 `;
